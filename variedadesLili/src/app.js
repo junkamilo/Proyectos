737ff8e292d4f946dca94396dcf3cd466bbe350d
Juan camilo <juancamilo.campobarrios@gmail.com>
2025-11-07 23:25:17 -0500

    preparando frontend

--- variedadesLili/src/app.js
+++
@@ -1,9 +0,0 @@
-import formulario from "./components/form";
-
-const app = document.getElementById("app");
-
-//instanciamos el formulario
-const form = await formulario();
-
-//lo pegamos a su elemento padre
-app.append(form);
